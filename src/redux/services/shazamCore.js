ea68836bb3d77f19cdbf5c7b898312a46ddb0eed
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-10-07 10:28:45 +0530

    API Fetch Logic created

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", process.env.REACT_APP_RAPID_API_KEY);
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
