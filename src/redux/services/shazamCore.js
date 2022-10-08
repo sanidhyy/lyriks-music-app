3b5a7743f4791a248ce40baebdf00603842f6cbd
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-10-08 18:05:26 +0530

    search functionality added

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -27,6 +27,10 @@ export const shazamCoreApi = createApi({
     getSongsByCountry: builder.query({
       query: (countryCode) => `/charts/country?country_code=${countryCode}`,
     }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
   }),
 });
 
@@ -37,4 +41,5 @@ export const {
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
   useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
 } = shazamCoreApi;
