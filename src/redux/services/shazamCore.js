f69c86cf52fdc7ad325406f99e2b67d157bd21c1
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-10-08 14:36:14 +0530

    Lyrics and song info added

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,7 +12,10 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
